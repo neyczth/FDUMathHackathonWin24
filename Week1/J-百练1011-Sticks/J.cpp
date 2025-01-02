82efde227c18e6575a8193b9b226c61ddc125019
neyczth <neyczth@126.com>
2025-01-02 11:56:08 +0800

    update Problem J

--- Week1/J-百练1011-Sticks/J.cpp
+++ Week1/J-百练1011-Sticks/J.cpp
@@ -9,17 +9,17 @@ vector<bool> vst(MAX_STICK, 0);
 int sum_length = 0, trial_length = 0;
 int num_sticks;
 
-bool backtracking(const int &num_corr_stick, const int &corr_length) {
+bool backtracking(const int &num_corr_stick, const int &corr_length, const int &start_idx) {
     if (num_corr_stick >= sum_length / trial_length) return true;
 
     bool is_ok = 0;
-    for (int i = 0; i < num_sticks; i++) {
-        int &&rest_length = trial_length - corr_length;
-        int last_trial = -1;
+    int &&rest_length = trial_length - corr_length;
+    int last_trial = -1;
+    for (int i = start_idx; i < num_sticks; i++) {
         if (!vst[i] && sticks[i] <= rest_length && sticks[i] != last_trial) {
             vst[i] = true; last_trial = sticks[i];
-            if (rest_length == sticks[i]) is_ok |= backtracking(num_corr_stick+1, 0);
-            else is_ok |= backtracking(num_corr_stick, corr_length + sticks[i]);
+            if (rest_length == sticks[i]) is_ok |= backtracking(num_corr_stick+1, 0, 0);
+            else is_ok |= backtracking(num_corr_stick, corr_length + sticks[i], i+1);
             if (is_ok) return true;
             vst[i] = false;
             if (corr_length == 0 || rest_length == sticks[i]) break;
@@ -50,7 +50,7 @@ int main() {
 
         for (trial_length = max_length; trial_length <= sum_length; trial_length++) {
             if (sum_length % trial_length != 0) continue;
-            if (backtracking(0, 0)) {
+            if (backtracking(0, 0, 0)) {
                 cout << trial_length << endl;
                 break;
             }
