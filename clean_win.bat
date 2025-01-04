a4d5d3b0451dff982591ab0f60526bc4b668ff37
neyczth <neyczth@126.com>
2025-01-04 09:53:08 +0800

    update

--- clean_win.bat
+++ clean_win.bat
@@ -3,12 +3,10 @@ setlocal enabledelayedexpansion
 
 for /r %%f in (*.exe *.in *.out) do (
     del "%%f"
-    echo Deleted "%%f"
 )
 
 for /r %%d in (.) do (
     if exist "%%d\.cph\" (
         rmdir /s /q "%%d\.cph\"
-        echo Deleted "%%d\.cph\"
     )
 )
