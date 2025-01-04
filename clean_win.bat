926519a4d0762521718127e9ede6210b980b0481
neyczth <neyczth@126.com>
2025-01-04 09:59:45 +0800

    update clean_win.bat

--- clean_win.bat
+++ clean_win.bat
@@ -3,10 +3,13 @@ setlocal enabledelayedexpansion
 
 for /r %%f in (*.exe *.in *.out) do (
     del "%%f"
+    echo Deleted "%%f"
 )
 
 for /r %%d in (.) do (
     if exist "%%d\.cph\" (
         rmdir /s /q "%%d\.cph\"
+        set "dirpath=%%d"
+        echo Deleted "!dirpath:~0,-2!\.cph\"
     )
 )
